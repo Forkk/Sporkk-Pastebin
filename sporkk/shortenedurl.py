b322cc1a72929f0964145dde3dec350b927b4ba9
Andrew <forkk@forkk.net>
2013-04-10 14:58:09 -0500

    Implement jQuery/AJAX for URL shortener.

--- sporkk/shortenedurl.py
+++ sporkk/shortenedurl.py
@@ -14,13 +14,15 @@
 
 from . import app, db
 
-from flask import render_template, redirect, abort, url_for
+from flask import render_template, redirect, abort, url_for, request
 
 from urltype import URLType, URLTypeSubmitForm, err_id_map
 from urlmodel import URLMapping, generate_unused_url_id
 from postermodel import PosterModel, get_poster_timestamp, update_poster_timestamp
 
 from datetime import datetime, timedelta
+
+import json
 
 import re
 
@@ -41,18 +43,8 @@ class ShortenedURLType(URLType):
 	def get_submit_form_info(self):
 		return URLTypeSubmitForm("shortener-form.html", "shorten", "URL Shortener")
 
-	def handle_submit_form(self, form_info_list, error_id):
-		errormsg = None
-		if error_id is None:
-			errormsg = None
-		elif error_id in local_err_id_map:
-			errormsg = local_err_id_map[error_id]
-		elif error_id in err_id_map:
-			errormsg = err_id_map[error_id]
-		else:
-			errormsg = "An unknown error occurred."
-
-		return render_template("shortener-form.html", submit_forms = form_info_list, error = errormsg)
+	def handle_submit_form(self, form_info_list, unused):
+		return render_template("shortener-form.html", submit_forms = form_info_list)
 
 	def handle_view(self, url_id, shorturl, error = None):
 		if shorturl is None or type(shorturl) is not ShortenedURLModel:
@@ -60,23 +52,27 @@ class ShortenedURLType(URLType):
 
 		return redirect(shorturl.mapped_url)
 
-	def handle_submit(self, request):
+
+	def handle_submit(self, request, return_json = False):
+		# POST fields: longurl
+
 		longurl = request.form['longurl']
 
 		# If the long URL is not a valid URL.
 		if not urlregex.search(longurl):
-			return redirect(self.error_url('bad-longurl'))
+			return self.shorten_fail("bad-longurl", return_json)
 
 		# If prevent dupe IDs is enabled, check if this URL has already been shortened.
 		if app.config.get('SHORTEN_PREVENT_DUPE_IDS'):
 			shorturl = get_by_mapped_url(longurl)
 			if shorturl is not None:
 				# If this URL has already been shortened, give the user the already shortened URL, rather than creating a new shortened URL for it.
-				return render_template("shorten-success.html", shortened_url = url_for('url_id_view', url_id = shorturl.url_id))
+				return self.shorten_success(shorturl, return_json)
+
 
 		user_lastpost = get_poster_timestamp(request.remote_addr)
 		if user_lastpost is not None and datetime.utcnow() < user_lastpost + timedelta(seconds = app.config.get('POST_COOLDOWN_TIME')):
-			return redirect(self.error_url('too-fast'))
+			return self.shorten_fail("too-fast", return_json)
 
 		# Generate a random string for the URL ID. Make sure it's not in use.
 		url_id = generate_unused_url_id(app.config.get('SHORTURL_LENGTH'))
@@ -93,7 +89,26 @@ class ShortenedURLType(URLType):
 
 		db.session.commit()
 
-		return render_template("shorten-success.html", shortened_url = url_for('url_id_view', url_id = shorturl.url_id))
+		return self.shorten_success(shorturl, return_json)
+		
+
+	def shorten_success(self, shorturl, return_json = False):
+		if return_json:
+			return json.dumps({ "error": False, "url": shorturl.url_id, })
+		else:
+			return render_template("shorten-success.html", shortened_url = url_for('url_id_view', url_id = shorturl.url_id))
+
+	def shorten_fail(self, error_id, return_json = False):
+		if return_json:
+			return json.dumps({ "error": True, "errorType": error_id, })
+		else:
+			if error_id in local_err_id_map:
+				return local_err_id_map[error_id]
+			elif error_id in err_id_map:
+				return err_id_map[error_id]
+			else:
+				return "An unknown error occurred."
+
 
 	def get_model_type(self):
 		return ShortenedURLModel
@@ -124,5 +139,10 @@ def get_by_mapped_url(mapped_url):
 	"""Finds a shortened URL in the database whose mapped URL matches the given URL."""
 	return ShortenedURLModel.query.filter_by(mapped_url = mapped_url).first()
 
+shorturl_type = ShortenedURLType()
 
-url_types = [ ShortenedURLType() ]
+url_types = [ shorturl_type ]
+
+@app.route('/short/submit/json', methods = ['POST'])
+def handle_submit_ajax():
+	return shorturl_type.handle_submit(request, True)
